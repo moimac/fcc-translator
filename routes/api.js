610e5262eb2e855adb5651291300fabc58be99e5
moymac <lameraweb@hotmail.com>
2024-06-25 22:39:04 -0400

    Super dirty code. All FCC tests pass. Unit tests written. Functional tests missing.

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,26 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
+  const localeOptions = ["american-to-british", "british-to-american"];
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    if (text === null || text === undefined || !locale)
+      return res.send({ error: "Required field(s) missing" });
+    if (text.length === 0) return res.send({ error: "No text to translate" });
+    if (!localeOptions.includes(locale))
+      return res.send({ error: "Invalid value for locale field" });
+
+    let translation;
+    if (locale === localeOptions[0]) {
+      translation = translator.translateAtoB(text);
+    } else if (locale === localeOptions[1]) {
+      translation = translator.translateBtoA(text);
+    }
+    console.log(translation);
+    return res.send({ text, translation });
+  });
 };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,190 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  // Translate Mangoes are my favorite fruit. to British English
+  test("Translate Mangoes are my favorite fruit. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("Mangoes are my favorite fruit."),
+      `Mangoes are my <span class="highlight">favourite</span> fruit.`
+    );
+  });
 
+  // Translate I ate yogurt for breakfast. to British English
+  test("Translate I ate yogurt for breakfast. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("I ate yogurt for breakfast."),
+      `I ate <span class="highlight">yoghurt</span> for breakfast.`
+    );
+  });
+
+  // Translate We had a party at my friend's condo. to British English
+  test("Translate We had a party at my friend's condo. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("We had a party at my friend's condo."),
+      `We had a party at my friend's <span class="highlight">flat</span>.`
+    );
+  });
+
+  // Translate Can you toss this in the trashcan for me? to British English
+  test("Translate Can you toss this in the trashcan for me? to British English", () => {
+    assert.equal(
+      translator.translateAtoB("Can you toss this in the trashcan for me?"),
+      `Can you toss this in the <span class="highlight">bin</span> for me?`
+    );
+  });
+
+  // Translate The parking lot was full. to British English
+  test("Translate The parking lot was full. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("The parking lot was full."),
+      `The <span class="highlight">car park</span> was full.`
+    );
+  });
+
+  // Translate Like a high tech Rube Goldberg machine. to British English
+  test("Translate Like a high tech Rube Goldberg machine. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("Like a high tech Rube Goldberg machine."),
+      `Like a high tech <span class="highlight">Heath Robinson device</span>.`
+    );
+  });
+
+  // Translate To play hooky means to skip class or work. to British English
+  test("Translate To play hooky means to skip class or work. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("To play hooky means to skip class or work."),
+      `To <span class="highlight">bunk off</span> means to skip class or work.`
+    );
+  });
+
+  // Translate No Mr. Bond, I expect you to die. to British English
+  test("Translate No Mr. Bond, I expect you to die. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("No Mr. Bond, I expect you to die."),
+      `No <span class="highlight">Mr</span> Bond, I expect you to die.`
+    );
+  });
+  // Translate Dr. Grosh will see you now. to British English
+  test("Translate Dr. Grosh will see you now. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("Dr. Grosh will see you now."),
+      `<span class="highlight">Dr</span> Grosh will see you now.`
+    );
+  });
+  // Translate Lunch is at 12:15 today. to British English
+  test("Translate Lunch is at 12:15 today. to British English", () => {
+    assert.equal(
+      translator.translateAtoB("Lunch is at 12:15 today."),
+      `Lunch is at <span class="highlight">12.15</span> today.`
+    );
+  });
+  // Translate We watched the footie match for a while. to American English
+  test("Translate We watched the footie match for a while. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("We watched the footie match for a while."),
+      `We watched the <span class="highlight">soccer</span> match for a while.`
+    );
+  });
+
+  // Translate Paracetamol takes up to an hour to work. to American English
+  test("Translate Paracetamol takes up to an hour to work. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("Paracetamol takes up to an hour to work."),
+      `<span class="highlight">Tylenol</span> takes up to an hour to work.`
+    );
+  });
+  // Translate First, caramelise the onions. to American English
+  test("Translate First, caramelise the onions. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("First, caramelise the onions."),
+      `First, <span class="highlight">caramelize</span> the onions.`
+    );
+  });
+
+  // Translate I spent the bank holiday at the funfair. to American English
+  test("Translate I spent the bank holiday at the funfair. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("I spent the bank holiday at the funfair."),
+      `I spent the <span class="highlight">public holiday</span> at the <span class="highlight">carnival</span>.`
+    );
+  });
+
+  // Translate I had a bicky then went to the chippy. to American English
+  test("Translate I had a bicky then went to the chippy. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("I had a bicky then went to the chippy."),
+      `I had a <span class="highlight">cookie</span> then went to the <span class="highlight">fish-and-chip shop</span>.`
+    );
+  });
+
+  // Translate I've just got bits and bobs in my bum bag. to American English
+  test("Translate I've just got bits and bobs in my bum bag. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("I've just got bits and bobs in my bum bag."),
+      `I've just got <span class="highlight">odds and ends</span> in my <span class="highlight">fanny pack</span>.`
+    );
+  });
+  // Translate The car boot sale at Boxted Airfield was called off. to American English
+  test("Translate The car boot sale at Boxted Airfield was called off. to American English", () => {
+    assert.equal(
+      translator.translateBtoA(
+        "The car boot sale at Boxted Airfield was called off."
+      ),
+      'The <span class="highlight">swap meet</span> at Boxted Airfield was called off.'
+    );
+  });
+  // Translate Have you met Mrs Kalyani? to American English
+  test("Translate Have you met Mrs Kalyani? to American English", () => {
+    assert.equal(
+      translator.translateBtoA("Have you met Mrs Kalyani?"),
+      'Have you met <span class="highlight">Mrs.</span> Kalyani?'
+    );
+  });
+
+  // Translate Prof Joyner of King's College, London. to American English
+  test("Translate Prof Joyner of King's College, London. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("Prof Joyner of King's College, London."),
+      `<span class="highlight">Prof.</span> Joyner of King's College, London.`
+    );
+  });
+  // Translate Tea time is usually around 4 or 4.30. to American English
+  test("Translate Tea time is usually around 4 or 4.30. to American English", () => {
+    assert.equal(
+      translator.translateBtoA("Tea time is usually around 4 or 4.30."),
+      'Tea time is usually around 4 or <span class="highlight">4:30</span>.'
+    );
+  });
+  // Highlight translation in Mangoes are my favorite fruit.
+  test("Highlight translation in Mangoes are my favorite fruit.", () => {
+    assert.equal(
+      translator.translateAtoB("Mangoes are my favorite fruit."),
+      'Mangoes are my <span class="highlight">favourite</span> fruit.'
+    );
+  });
+  // Highlight translation in I ate yogurt for breakfast.
+  test("Highlight translation in I ate yogurt for breakfast.", () => {
+    assert.equal(
+      translator.translateAtoB("I ate yogurt for breakfast."),
+      'I ate <span class="highlight">yoghurt</span> for breakfast.'
+    );
+  });
+  // Highlight translation in We watched the footie match for a while.
+  test("Highlight translation in We watched the footie match for a while.", () => {
+    assert.equal(
+      translator.translateBtoA("We watched the footie match for a while."),
+      'We watched the <span class="highlight">soccer</span> match for a while.'
+    );
+  });
+  // Highlight translation in Paracetamol takes up to an hour to work.
+  test("Highlight translation in Paracetamol takes up to an hour to work.", () => {
+    assert.equal(
+      translator.translateBtoA("Paracetamol takes up to an hour to work."),
+      '<span class="highlight">Tylenol</span> takes up to an hour to work.'
+    );
+  });
 });
